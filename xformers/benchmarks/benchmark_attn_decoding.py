8cf7c43594ef6a37d10b8cdd8614a37b49590ae4
Shucai Xiao <shucai.xiao@amd.com>
2025-07-29 20:38:58 -0500

    revert unnecessary changes

--- xformers/benchmarks/benchmark_attn_decoding.py
+++ xformers/benchmarks/benchmark_attn_decoding.py
@@ -30,8 +30,8 @@ CASES = [
         K=128,
         attn_bias_type=xops.fmha.attn_bias.BlockDiagonalCausalWithOffsetPaddedKeysMask,
     )
-    for i in range(8, 9)
-    for hkv in [2]
+    for i in range(8, 18)
+    for hkv in (1, 2)
 ]
 
 
@@ -347,21 +347,21 @@ BENCHMARKS: Dict[str, Type[AttentionDecodingBase]] = {
     "pytorch": AttentionDecodingPyTorchRepeat,
 }
 
-#if torch.version.cuda:
-#    BENCHMARKS["cutlass"] = AttentionDecodingCUTLASS
+if torch.version.cuda:
+    BENCHMARKS["cutlass"] = AttentionDecodingCUTLASS
 
-#if torch.version.hip:
-#    BENCHMARKS.update(
-#        {
-#            "ck": AttentionDecodingCK,
-#            "ck_splitK": AttentionDecodingCKSplitKV,
-#        }
-#    )
+if torch.version.hip:
+    BENCHMARKS.update(
+        {
+            "ck": AttentionDecodingCK,
+            "ck_splitK": AttentionDecodingCKSplitKV,
+        }
+    )
 
 
 if (sys.version_info.major, sys.version_info.minor) >= (3, 9):
     BENCHMARKS["triton_splitK"] = AttentionDecodingSplitKV
-#    BENCHMARKS["triton_int4KV"] = AttentionDecodingSplitInt4KV
+    BENCHMARKS["triton_int4KV"] = AttentionDecodingSplitInt4KV
 
 try:
     import flash_attn
